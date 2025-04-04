bcab14e35d99ba0d63aace4230e4f0d83f5dee96
Julez25 <julian0205.jt@gmail.com>
2025-04-04 14:38:09 +0200

    Added Win_List, was mostly thinking how to check if a player won

--- player.py
+++ player.py
@@ -1,12 +1,17 @@
 from enum import Enum
-
+from grid import Grid
 class Player(Enum):
     
-    X_User = "X"
-    O_User = "O"
-
-
+    x_User = "X"
+    o_User = "O"
+    win_lines = [
+        ["X","X","X"],
+        ["O","O","O"]
+        ]
     
+    def win_Con(self, list):
+        for 
+
 #print(Player.O)
 #print(Player.O.name)
-print(Player.X_User)
+#print(Player.O.value)
